d66e5d7f72307aff73cc2068fb9bbfbff6965c0d
Михаил <mishaprosvirninmail@yandex.ru>
2023-07-05 12:16:01 +0500

    dependencies added (nav component, fragment, Gson, retrofit, okhttp, rxJava, Viewmodel, Koin); navigation graph added; main fragment added

--- app/src/main/java/com/michael/courierassistant/presenter/ui/MainActivity.kt
+++ app/src/main/java/com/michael/courierassistant/presenter/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.michael.courierassistant
+package com.michael.courierassistant.presenter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.michael.courierassistant.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/michael/courierassistant/presenter/ui/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.michael.courierassistant.presenter.ui
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.michael.courierassistant.R
+import com.michael.courierassistant.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.text.setOnClickListener {
+            binding.text.setBackgroundColor(Color.BLUE)
+        }
+    }
+}
